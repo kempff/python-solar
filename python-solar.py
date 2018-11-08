1edff3437fa1d498f0eda95fbd7b712745bfb9ae
kempff <kempff@webmail.co.za>
2018-11-08 21:17:40 +0200

    Initial project setup

---
+++ python-solar.py
@@ -0,0 +1,6 @@
+# Python reader for solar panel controller
+
+import evdev
+devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+for device in devices:
+    print(device.fn, device.name, device.phys)
