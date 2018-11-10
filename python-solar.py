b73a8617330f6bfe494731d862cff344d3225970
kempff <kempff@webmail.co.za>
2018-11-10 15:27:11 +0200

    Added scheduler and logging

--- python-solar.py
+++ python-solar.py
@@ -1,6 +1,27 @@
 # Python reader for solar panel controller
 
 import evdev
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-for device in devices:
-    print(device.fn, device.name, device.phys)
+from influxdb import InfluxDBClient
+import schedule
+import logging
+import time
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
+
+def read_data():
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    for device in devices:
+        logging.info("{0} - {1} - {2} ".format(device.fn, device.name, device.phys))
+
+def populate_data():
+    logging.info("Writing to database...")
+
+# Read every 10 seconds =, write every 20
+schedule.every(20).seconds.do(read_data)
+schedule.every(30).seconds.do(populate_data)
+
+logging.info("Program starts")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
