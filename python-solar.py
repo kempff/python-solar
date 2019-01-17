a35758a2c110c9eced21a9a70a8652579b84ed79
kempff <kempff@webmail.co.za>
2019-01-17 22:54:24 +0200

    Attempt at writing mode data - cannot write a string though...

--- python-solar.py
+++ python-solar.py
@@ -6,6 +6,8 @@ import schedule
 import logging
 import time
 
+influx_client = InfluxDBClient('localhost', 8086, database='solardb')
+
 debug_data = True
 if debug_data:
     # Open the files for reading
@@ -15,6 +17,7 @@ if debug_data:
 mode_data = []          # From QMOD command
 status_data = []        # From QPIGS command
 ratings_data = []       # From QPIRI command
+
 
 # Read next line from each file, if at end of the file loop to the beginning
 def read_file_data(input_file):
@@ -30,6 +33,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=log
 
 # Read data function (either from USB or file)
 def read_data():
+    global mode_data
     if debug_data:
         mode_data = read_file_data(mod_file)
         logging.info("Mode data: " + mode_data)
@@ -40,12 +44,33 @@ def read_data():
         for device in devices:
             logging.info("{0} - {1} - {2} ".format(device.fn, device.name, device.phys))
 
+def populate_mode_data(mode_data, client):
+    mode_dictionary = { 'P': 'Power on', 'S': 'Standby', 'L': 'Line',
+                        'B': 'Battery', 'F': 'Fault', 'H': 'Power saving'}
+    mode_body = {
+        'measurement': 'system_mode',
+        'tags': {
+            'host': "test_installation1",
+            },
+        'fields': {
+            'mode': 'unknown'
+            }
+    }
+    if mode_data[0] in mode_dictionary.keys():
+        mode_body['fields']['mode'] = mode_dictionary[mode_data[0]]
+        client.write_points(mode_body)
+    else:
+        logging.error("Invalid mode {0}".format(mode_data[0]))
+
 def populate_data():
+    global mode_data
+    global influx_client
     logging.info("Writing to database...")
+    populate_mode_data(mode_data, influx_client)
 
 # Read every 20 seconds =, write every 30
 schedule.every(2).seconds.do(read_data)
-schedule.every(30).seconds.do(populate_data)
+schedule.every(5).seconds.do(populate_data)
 
 logging.info("Program starts")
 
