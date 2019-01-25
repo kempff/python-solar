2d5034d2d0bf5e15b1a346bf389e7be5fc9bc790
kempff <kempff@webmail.co.za>
2019-01-25 19:23:59 +0200

    Added config, ignore and grafana files

---
+++ solar_config.py
@@ -0,0 +1,8 @@
+import os
+
+LOG_FILE_PATH = os.getenv('LOG_FILE_PATH','/home/pi/Projects/python-solar/log/')
+LOG_LEVEL = os.getenv('LOG_LEVEL','WARNING')
+INFLUX_HOST = os.getenv('INFLUX_HOST','localhost')
+INFLUX_PORT = os.getenv('INFLUX_PORT','8086')
+INFLUX_DB = os.getenv('INFLUX_DATABASE','solardb')
+DEBUG_MODE = os.getenv('DEBUG_MODE', False)
