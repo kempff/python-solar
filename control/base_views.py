eb30064d315991721f349e3387f78ec2335cc668
kempff@webmail.co.za <kempff@webmail.co.za>
2020-05-08 22:16:36 +0200

    Started with attempt to add config to frontend

--- control/base_views.py
+++ control/base_views.py
@@ -18,10 +18,24 @@ from solar.settings import APP_NAME, APP_VERSION
 the_logger = StructLogger()
 the_logger.info(f'Running {APP_NAME} version {APP_VERSION}')
 
+pv_charge_current = None
+ac_charge_current = None
+battery_recharge_v = None
+battery_re_discharge_v = None
+
+
 class HomePageView(View):
+    global pv_charge_current
+    global ac_charge_current
+    global battery_recharge_v
+    global battery_re_discharge_v
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
         else:
+            pv_charge_current = self.request.GET.get('pv_charge_current')
+            ac_charge_current = self.request.GET.get('ac_charge_current')
+            battery_recharge_v = self.request.GET.get('battery_recharge_v')
+            battery_re_discharge_v = self.request.GET.get('battery_re_discharge_v')
             return render(request, "home.html")
 
