968f7feecd95031643079b9f7077ddec13579d24
kempff@webmail.co.za <kempff@webmail.co.za>
2020-07-06 21:53:18 +0200

    Auto load home page Use bootstrap table with hover

--- control/urls.py
+++ control/urls.py
@@ -6,5 +6,6 @@ from .control_views import ControlView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('home', HomePageView.as_view(), name='home'),
     path('control', ControlView.as_view(), name='control'),
 ]
