1c075f569abd4ae476202bd88387417b81c57754
kempff <kempff@webmail.co.za>
2020-07-11 11:58:23 +0200

    Updated versions for release

--- solar/settings.py
+++ solar/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Application name and version
-APP_VERSION = "0.0.1"                       # Ensure this is the same as the Git release tag version
-APP_NAME = "Python_Solar"            # Don't use spaces or non-letters
+APP_VERSION = "0.1.0"                       # Ensure this is the same as the Git release tag version
+APP_NAME = "Web_Solar"            # Don't use spaces or non-letters
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
